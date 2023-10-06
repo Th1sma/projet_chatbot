5871e8f9c67fc850f20d8adbc33069959a3f2519
Thisma17 <mathis.niveau17@outlook.com>
2023-10-06 20:57:03 +0200

    UPDATE : correction de code

--- chatbot/js/responses.js
+++ chatbot/js/responses.js
@@ -1,98 +1,93 @@
-var eth;    // ethereum
-var btc;    // bitcoin
-var xrp;    // Ripple
-var binance;    //binance
-var polka;   // polkadot
-var card;    // cardano
+var eth; // ethereum
+var btc; // bitcoin
+var xrp; // Ripple
+var binance; //binance
+var polka; // polkadot
+var card; // cardano
 
 // -------------- Request for Ethereum -------------- //
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
-    .then(res => res.json())
-    .then(data => eth = (data["ethereum"].usd))
+fetch(
+  "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+)
+  .then((res) => res.json())
+  .then((data) => (eth = data["ethereum"].usd));
 
 // -------------- Request for Bitcoin -------------- //
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
-    .then(res => res.json())
-    .then(data => btc = (data["bitcoin"].usd))
+fetch(
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+)
+  .then((res) => res.json())
+  .then((data) => (btc = data["bitcoin"].usd));
 
 // -------------- Request for Ripple -------------- //
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=ripple&vs_currencies=usd')
-    .then(res => res.json())
-    .then(data => xrp = (data["ripple"].usd))
+fetch(
+  "https://api.coingecko.com/api/v3/simple/price?ids=ripple&vs_currencies=usd"
+)
+  .then((res) => res.json())
+  .then((data) => (xrp = data["ripple"].usd));
 
 // -------------- Request for Binance -------------- //
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd')
-    .then(res => res.json())
-    .then(data => binance = (data["binancecoin"].usd))
+fetch(
+  "https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd"
+)
+  .then((res) => res.json())
+  .then((data) => (binance = data["binancecoin"].usd));
 
 // -------------- Request for Cardano -------------- //
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd')
-    .then(res => res.json())
-    .then(data => card = (data["cardano"].usd))
+fetch(
+  "https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd"
+)
+  .then((res) => res.json())
+  .then((data) => (card = data["cardano"].usd));
 
-    // -------------- Request for polkadot -------------- //
-fetch('https://api.coingecko.com/api/v3/simple/price?ids=polkadot&vs_currencies=usd')
-    .then(res => res.json())
-    .then(data => polka = (data["polkadot"].usd))
-
-    
+// -------------- Request for polkadot -------------- //
+fetch(
+  "https://api.coingecko.com/api/v3/simple/price?ids=polkadot&vs_currencies=usd"
+)
+  .then((res) => res.json())
+  .then((data) => (polka = data["polkadot"].usd));
 
 function getBotResponse(input) {
+  let name;
+  let inputText = input.toLowerCase();
+  let inputName = inputText;
 
-    let name;
-    let inputText = input.toLowerCase();
-    let inputName = inputText;
-
-    // discussion 
-    if (inputText == "hello" || inputText == "hey") {
-
-        return "Hello human, Nice to meet you !";
-
-    }else if (inputText == "how are you ?") {
- 
-        return "I'm always happy, and you ?";
-    
-    }else if (inputText == "fine" || inputText == "very good" || inputText == "fine" || inputText == "not well") {
- 
-        return "Ok bro, how can i be help you ?";
-
-    } else if (inputText == "gus help me" || inputText == "help me") {
-
-        return "Yes of course ! <br> I can give you some information of cryptocurrency. <br> Write : <b> \"list\" </b> for the list of cryptocurrency.";
-
-    } else if (inputText == "list") {
-
-        return "Here is the list of cryptocurrency on which I can inform you :<b> bitcoin, binance-coin, cardano, ethereum, polkadot, xrp(ripple)."
-      
-    }else if(inputText == "ethereum"){      // price of ethereum
-
-        return "<b>Ethereum : </b>" + eth + " $";
-
-    }else if(inputText == "bitcoin"){       // price of Bitcoin
-
-        return "<b>Bitcoin :</b> " + btc + " $";
-    
-    }else if(inputText == "xrp" || inputText == "ripple"){      //price of Ripple
-
-        return "<b>Ripple : </b>" + xrp + " $";
-    
-    }else if(inputText == "binance" || inputText == "binance coin"){        //price of binance coin
-
-        return "<b>Binance Coin : </b>" + binance + " $";
-
-    }else if(inputText == "cardano"){        //price of cardano
-
-        return "<b>Cardano : </b>" + card + " $";
-
-    }else if(inputText == "polkadot"){        //price of polkadot
-
-        return "<b>Polkadot : </b>" + polka + " $";
-
-    }else{      // if, input no accepted
-
-        return "sorry, i don't understand... <br> If you don't know what to say, check my documentation.";
-    }
-
+  // discussion
+  if (inputText == "hello" || inputText == "hey") {
+    return "Hello human, Nice to meet you !";
+  } else if (inputText == "how are you ?") {
+    return "I'm always happy, and you ?";
+  } else if (
+    inputText == "fine" ||
+    inputText == "very good" ||
+    inputText == "fine" ||
+    inputText == "not well"
+  ) {
+    return "Ok bro, how can i be help you ?";
+  } else if (inputText == "gus help me" || inputText == "help me") {
+    return 'Yes of course ! <br> I can give you some information of cryptocurrency. <br> Write : <b> "list" </b> for the list of cryptocurrency.';
+  } else if (inputText == "list") {
+    return "Here is the list of cryptocurrency on which I can inform you :<b> bitcoin, binance-coin, cardano, ethereum, polkadot, xrp(ripple).";
+  } else if (inputText == "ethereum") {
+    // price of ethereum
+    return "<b>Ethereum : </b>" + eth + " $";
+  } else if (inputText == "bitcoin") {
+    // price of Bitcoin
+    return "<b>Bitcoin :</b> " + btc + " $";
+  } else if (inputText == "xrp" || inputText == "ripple") {
+    //price of Ripple
+    return "<b>Ripple : </b>" + xrp + " $";
+  } else if (inputText == "binance" || inputText == "binance coin") {
+    //price of binance coin
+    return "<b>Binance Coin : </b>" + binance + " $";
+  } else if (inputText == "cardano") {
+    //price of cardano
+    return "<b>Cardano : </b>" + card + " $";
+  } else if (inputText == "polkadot") {
+    //price of polkadot
+    return "<b>Polkadot : </b>" + polka + " $";
+  } else {
+    // if, input no accepted
+    return "sorry, i don't understand... <br> If you don't know what to say, check my documentation.";
+  }
 }
- 
-    
--- chatbot/js/script.js
+++ chatbot/js/script.js
@@ -1,114 +1,106 @@
-
 // Collapsible
 var coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
 
-        var content = this.nextElementSibling;
+    var content = this.nextElementSibling;
 
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-
-    });
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
 
 function getTime() {
-    let today = new Date();
-    hours = today.getHours();
-    minutes = today.getMinutes();
+  let today = new Date();
+  hours = today.getHours();
+  minutes = today.getMinutes();
 
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
 
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
 
-    let time = hours + ":" + minutes;
-    return time;
+  let time = hours + ":" + minutes;
+  return time;
 }
 
 // Gets the first message
 function firstBotMessage() {
-    let firstMessage = "He is ready to answer you ! <br> if you have need more information for talk with GUS, <b>Download the notice <b>"
-    document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+  let firstMessage =
+    "He is ready to answer you ! <br> if you have need more information for talk with GUS, <b>Download the notice <b>";
+  document.getElementById("botStarterMessage").innerHTML =
+    '<p class="botText"><span>' + firstMessage + "</span></p>";
 
-    let time = getTime();
+  let time = getTime();
 
-    $("#chat-timestamp").append(time);
-    document.getElementById("userInput").scrollIntoView(false);
+  $("#chat-timestamp").append(time);
+  document.getElementById("userInput").scrollIntoView(false);
 }
 
 firstBotMessage();
 
 // Retrieves the response
 function getHardResponse(userText) {
+  let botResponse = getBotResponse(userText).toLowerCase(userText);
 
-    let botResponse = getBotResponse(userText).toLowerCase(userText);
+  let botHtml = '<p class="botText"><span>' + botResponse + "</span></p>";
+  $("#chatbox").append(botHtml);
 
-    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
-    $("#chatbox").append(botHtml);
-
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 }
 
 //Gets the text text from the input box and processes it
 function getResponse() {
-    let userText = $("#textInput").val();
+  let userText = $("#textInput").val();
 
-    if (userText == "") {
-        return 0;
-    }
+  if (userText == "") {
+    return 0;
+  }
 
-    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+  let userHtml = '<p class="userText"><span>' + userText + "</span></p>";
 
-    $("#textInput").val("");
-    $("#chatbox").append(userHtml);
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
-    setTimeout(() => {
-        getHardResponse(userText);
-    }, 1000)
-
+  setTimeout(() => {
+    getHardResponse(userText);
+  }, 1000);
 }
 
 // Handles sending text via button clicks
 function buttonSendText(sampleText) {
-    let userHtml = '<p class="botText"><span>' + sampleText + '</span></p>';
+  let userHtml = '<p class="botText"><span>' + sampleText + "</span></p>";
 
-    $("#userText").val("");
-    $("#chatbox").append(userHtml);
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
-
-    //Uncomment this if you want the bot to respond to this buttonSendText event
-    // setTimeout(() => {
-    //     getHardResponse(sampleText);
-    // }, 1000)
+  $("#userText").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 }
 
 function sendButton() {
-    getResponse();
+  getResponse();
 }
 
 function moneyButton() {
-    let text = "Click here !";
-    let URL = "https://www.youtube.com/watch?v=n1apQ00sxNs";
-    let message = "<b>Good job</b>, you have find the secret button ! "
+  let text = "Click here !";
+  let URL = "https://www.youtube.com/watch?v=n1apQ00sxNs";
+  let message = "<b>Good job</b>, you have find the secret button ! ";
 
-    //document.getElementById("botText").innerHTML = '<p class="botText"><span>' + message + text.link(URL) + '</span></p>';
-    buttonSendText(message + text.link(URL));
+  //document.getElementById("botText").innerHTML = '<p class="botText"><span>' + message + text.link(URL) + '</span></p>';
+  buttonSendText(message + text.link(URL));
 }
 
 // Press enter to send a message
 $("#textInput").keypress(function (e) {
-    if (e.which == 13) {
-        getResponse();
-    }
+  if (e.which == 13) {
+    getResponse();
+  }
 });
-
